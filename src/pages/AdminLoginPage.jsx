cd1872b483172f9861bfdc346a9309f4e13004b0
Panduranga <panduranga1797@gmail.com>
2026-07-31 19:07:43 +0530

    first commit

--- src/pages/AdminLoginPage.jsx
+++ src/pages/AdminLoginPage.jsx
@@ -15,7 +15,7 @@ export default function AdminLoginPage({ setCurrentUser }) {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
+      const res = await fetch("https://turingwings-backend.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
